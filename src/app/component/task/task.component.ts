86abe5a352bc7e8d92a44aa3d3a406aa06293fdd
Ronald <email@email.com>
2023-02-27 14:06:53 -0500

    Implementación UI Modals Logic: agregar, editar y eliminar.

--- src/app/component/task/task.component.ts
+++ src/app/component/task/task.component.ts
@@ -12,10 +12,10 @@ export class TaskComponent implements OnInit {
   constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.retrieveTutorials();
+    this.retrieveTasks();
   }
 
-  retrieveTutorials(): void {
+  retrieveTasks(): void {
     this.taskService.getAll()
       .subscribe(//to be notified whenever some data comes back from the server; OK or Exception
         data => {
@@ -25,5 +25,32 @@ export class TaskComponent implements OnInit {
         error => {
           console.log(error);
         });
+  }
+
+  public onOpenModal(task: Task, mode: string): void { //CrUD, which modal to open
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal'); //data-target -> id of the modal
+    }
+    if (mode === 'edit') {
+      //this.editEmployee = employee;
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      //this.deleteEmployee = employee;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    //if (container) {
+    //this.deleteEmployee = employee;
+    //button.setAttribute('data-target', '#deleteEmployeeModal');
+    //container.appendChild(button);
+    //container!.appendChild(button);
+    //}
+    container?.appendChild(button);
+    button.click();
   }
 }
